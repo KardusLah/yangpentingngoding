1d37bf24497be72e5085164ea6f0a2846318527f
KardusLah <bocahkardus999@gmail.com>
2025-05-24 23:42:37 +0700

    feat: update KaryawanSeeder and UserSeeder to use updateOrCreate; add additional fields for Karyawan

--- database/seeders/KaryawanSeeder.php
+++ database/seeders/KaryawanSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Karyawan;
 
 class KaryawanSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class KaryawanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $adminUser = User::where('email', 'bocahkardus999@email.com')->first();
+
+        if ($adminUser) {
+            Karyawan::updateOrCreate(
+                ['id_user' => $adminUser->id],
+                [
+                    'nama_karyawan' => 'Admin',
+                    'alamat' => 'Alamat Admin',
+                    'foto' => null,
+                    'no_hp' => '08123456789',
+                    'jabatan' => 'administrasi',
+                ]
+            );
+            // Tambahkan log untuk debug
+            echo "Karyawan admin dibuat/diupdate dengan id_user: " . $adminUser->id . PHP_EOL;
+        } else {
+            echo "User admin tidak ditemukan!" . PHP_EOL;
+        }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,14 +9,16 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@email.com',
-            'password' => bcrypt('12312312'),
-            'no_hp' => '08123456789',
-            'level' => 'admin',
-            'aktif' => 1,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'bocahkardus999@email.com'],
+            [
+                'name' => 'Admin',
+                'password' => bcrypt('12312312'),
+                'no_hp' => '08123456789',
+                'level' => 'admin',
+                'aktif' => 1,
+            ]
+        );
         // Tambahkan data lain sesuai kebutuhan
     }
 }
